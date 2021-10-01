75f51984a94ab4ff6ba483ea245928a05cbeada3
Yuno222 <91469826+Yuno222@users.noreply.github.com>
2021-10-01 11:52:13 +0900

    Update function.php

--- function.php
+++ function.php
@@ -3,8 +3,10 @@ function h($str){
     return htmlspecialchars($str,ENT_QUOTES,"UTF-8");
 }
 
+//ログイン状態をチェック、セッションの開始、クリックジャッキング対策。
 function login_check(){
     session_start();
+    header("X-FRAME-OPTIONS: DENY");
     if(!isset($_SESSION["user_id"])){
       header("Location:https://recordideas.herokuapp.com/index.php");
     }
