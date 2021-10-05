4b0c0a39e9396a7bba03711d9d64677faddd6684
Yuno222 <91469826+Yuno222@users.noreply.github.com>
2021-10-05 09:36:26 +0900

    Update good_ref.php

--- talkcontents/good_ref.php
+++ talkcontents/good_ref.php
@@ -2,10 +2,12 @@
 require("../dbconnect.php");
 require("../function.php");
 
+logincheck();
+
 if(!empty($_POST["idea_id"])){
     $idea_id=$_POST["idea_id"];
     
-    //いいねされている時は取り消す
+    //いいねされている時(activeクラスを持っている時)は取り消して処理を終える
     if($_POST["nowstate"]==="true"){
         $delete=$db->query("DELETE FROM evaluation WHERE idea_id=$idea_id ORDER BY created DESC LIMIT 1");
         $count=$db->query("SELECT count(*) as good_num FROM evaluation WHERE idea_id=$idea_id");
@@ -14,7 +16,7 @@ if(!empty($_POST["idea_id"])){
         exit();
     }
     
-    //いいね挿入
+    //いいねされていない時はいいね挿入
     $db->query("INSERT INTO evaluation VALUES(0,$idea_id,NOW())");
     $count=$db->query("SELECT count(*) as good_num FROM evaluation WHERE idea_id=$idea_id");
     $good_num=$count->fetch();
